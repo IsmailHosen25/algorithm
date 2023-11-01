2367ab02c94cc317e08d162eb667b955ef186c12
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-11-01 11:39:53 +0600

    mergesort added

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,17 +1,12 @@
-def sortedarr(a):
-
-    for i in range(1,len(a)):
-      
-      key=a[i]
-
-      j=i-1
-      while j>-1 and  a[j]>key:
-         a[j+1]=a[j]
-         j=j-1
-      a[j+1]=key
-    
-
-arr=[1,4,2,6,3,8,2,1]
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>key:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+arr=[2,1,6,1,45678,8,2,8,4,6,9,4]
 print(arr)
-sortedarr(arr)
+insertion_sort(arr)
 print(arr)
